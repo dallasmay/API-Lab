0722bbf399cb5a44b99c57d140515daae4e5d734
dallasmay <dmmay9@gmail.com>
2022-06-15 16:46:31 -0600

    completed front end part of lab

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const getResidentBtn = document.querySelector("button");
 
 const btnClick = () => {
-    console.log("Button clicked");
+    axios.get("https://swapi.dev/api/planets/2")
+    .then(res => {
+        console.log(res);
+        for (let i = 0; i < res.data.residents.length; i++) {
+            let resArr = res.data.residents;
+            console.log(resArr[i]);
+            axios.get(resArr[i])
+            .then(res => {
+                let newH2 = document.createElement("h2");
+                newH2.textContent = res.data.name;
+                document.querySelector("body").appendChild(newH2);
+            })
+        }
+    })
 }
 
-document.addEventListener("click", btnClick);
+getResidentBtn.addEventListener("click", btnClick);
